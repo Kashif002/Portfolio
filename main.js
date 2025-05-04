3d6945b5e48faf07377c95ad95bf9a777e8d549f
Kashif Anjum <83899339+Kashif002@users.noreply.github.com>
2025-05-04 20:00:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["AI Developer, ML Engineer, Data Scienctist, Progrmmer"],
+    strings: ["AI Developer, ML Engineer, Data Scientist, Programmer"],
     typeSpeed : 10,
     backSpeed : 100,
     backDelay : 1000,
